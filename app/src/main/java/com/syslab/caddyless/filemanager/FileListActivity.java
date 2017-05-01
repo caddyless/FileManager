c2d5fa8c756434d6c37e06c446f551d0b564c358
caddyless <874900712@qq.com>
2017-05-01 21:45:26 +0800

    most function have been added,but there is still a fatal error in this code,it will be fixed as soon as possible

---
+++ app/src/main/java/com/syslab/caddyless/filemanager/FileListActivity.java
@@ -0,0 +1,12 @@
+package com.syslab.caddyless.filemanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FileListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/syslab/caddyless/filemanager/MessageAdapter.java
@@ -0,0 +1,8 @@
+package com.syslab.caddyless.filemanager;
+
+/**
+ * Created by caddyless on 2017/4/21.
+ */
+
+public class MessageAdapter {
+}
